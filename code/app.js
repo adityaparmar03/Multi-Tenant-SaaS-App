488e96771cf4ad090b954cf12f781cd351258b40
Aditya Parmar <parmar415@gmail.com>
2017-04-16 16:43:28 -0700

    Add file upload functionality and work on UI

--- code/app.js
+++ code/app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 var ejs = require('ejs');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+//var users = require('./routes/users');
 
 var app = express();
 
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- code/routes/index.js
+++ code/routes/index.js
@@ -1,9 +1,55 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+var path = require('path');
+var formidable = require('formidable');
+var fs = require('fs');
+//var datetime = new Date();
+
+
+router.use(express.static(path.join(__dirname, 'public')));
+
+
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
+    //res.sendFile(path.join(__dirname, 'views/index.html'));
 });
+
+
+router.post('/upload', function(req, res){
+
+    // create an incoming form object
+    var form = new formidable.IncomingForm();
+    var datetime = new Date();
+
+    // specify that we want to allow the user to upload multiple files in a single request
+    form.multiples = true;
+
+    // store all uploads in the /uploads directory
+    form.uploadDir = path.join(__dirname, '/uploads');
+
+    // every time a file has been uploaded successfully,
+    // rename it to it's orignal name
+    form.on('file', function(field, file) {
+        fs.rename(file.path, path.join(form.uploadDir,datetime+"_"+file.name));
+    });
+
+    // log any errors that occur
+    form.on('error', function(err) {
+        console.log('An error has occured: \n' + err);
+    });
+
+    // once all the files have been uploaded, send a response to the client
+    form.on('end', function() {
+        res.end('success');
+    });
+
+    // parse the incoming request containing the form data
+    form.parse(req);
+    
+
+});
+
 
 module.exports = router;
